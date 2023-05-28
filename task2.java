69f2a5c3a7b85585ac72fee8979908484b32b8a7
eraymeric2016 <125488259+eraymeric2016@users.noreply.github.com>
2023-05-28 14:11:29 +0300

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,69 @@
+package week_03;
+
+/*
+Create a program that will define two double variables.
+ Take the two numbers are perform all the mathematical
+ operations on them ( + - * / % )
+ Output in this format:
+  num1 operator num2 = result
+
+Ex:
+num1 = 5
+num2 = 2
+
+output:
+5 + 2 = 7
+5 - 2 = 3
+
+
+public class MathOperations {
+public static void main(String[] args) {
+double num1 = 5;
+double num2 = 2;
+
+// Toplama
+double sum = num1 + num2;
+System.out.println(num1 + " + " + num2 + " = " + sum);
+
+// Çıkarma
+double diff = num1 - num2;
+System.out.println(num1 + " - " + num2 + " = " + diff);
+
+// Çarpma
+double mult = num1 * num2;
+System.out.println(num1 + " * " + num2 + " = " + mult);
+
+// Bölme
+double div = num1 / num2;
+System.out.println(num1 + " / " + num2 + " = " + div);
+
+// Mod alma
+double mod = num1 % num2;
+System.out.println(num1 + " % " + num2 + " = " + mod);
+}
+}
+*/
+public class task2 {
+    public static void main(String[] args) {
+
+
+        int num1=20;
+        int num2=5;
+        System.out.println(num1+"+"+num2 + " = " + (num1+num2));
+        System.out.println(num1+"-"+num2 + " = " + (num1-num2));
+        System.out.println(num1+"*"+num2 + " = " + (num1*num2));
+        System.out.println(num1+"/"+num2 + " = " + (num1/num2));
+        System.out.println(num1+"%"+num2 + " = " + (num1%num2));
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
